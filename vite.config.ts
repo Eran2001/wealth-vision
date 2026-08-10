6ce0c94810360fa6d2e39f35bb04f83f0d4ad810
Eran Hasareli <126740413+Eran2001@users.noreply.github.com>
2026-08-10 22:59:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-//
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
